02ed89d808075156199c75b54c7c899ea121eb57
blaine-t <github@bathost.net>
2022-08-19 12:45:59 -0500

    Initial settings page work

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,6 +1,8 @@
 import express, { NextFunction, Request, Response } from 'express'
 const router = express.Router()
 
+import db from '../lib/db'
+
 function checkAuthentication(req: Request, res: Response, next: NextFunction) {
   if (req.isAuthenticated()) {
     res.locals.user = req.user
@@ -20,6 +22,17 @@ router.get('/panel', (req, res) => {
 
 router.get('/settings', (req, res) => {
   res.render('pages/admin/settings')
+})
+
+router.post('/settings', (req, res) => {
+  if (
+    req.body.phone.match('^([2-9][0-8][0-9])[-]([2-9][0-9]{2})[-]([0-9]{4})$')
+  ) {
+    db.updateAdmin(res.locals.user.id, req.body.phone, false)
+    res.redirect('panel')
+    return
+  }
+  res.render('pages/admin/settings', { error: true })
 })
 
 router.get('/manage', (req, res) => {
