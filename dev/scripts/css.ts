e347a124e0ef781fa7e11c0b2391b9ef2a733e87
blaine-t <github@bathost.net>
2022-11-23 13:59:28 -0600

    Added some extra protections to CSS development

--- dev/scripts/css.ts
+++ dev/scripts/css.ts
@@ -1,17 +1,30 @@
 import shell from 'shelljs'
 
 const testFolder = './test'
+const backupFolder = './testBackup'
 
-const files = new Set(['.env, package.json', 'package-lock.json'])
+const files = new Set(['.env', 'package.json', 'package-lock.json'])
 
 const folders = new Set(['./public/', './dev/views/'])
 
-shell.rm('-R', testFolder)
+// Backup in case unsaved changes
+shell.rm('-R', backupFolder)
+shell.mv(testFolder, backupFolder)
 shell.mkdir(testFolder)
 
+// Move over TS file so it gets compiled properly
+shell.mkdir('./views')
+shell.mkdir('./views/components')
+shell.cp(
+  './dev/views/components/functions.ts',
+  './views/components/functions.ts'
+)
+
+// Build the typescript and populate dependencies
 shell.exec('npx tsc --outDir ./test')
 shell.exec('npm --prefix ./test i --omit=dev')
 
+// Copy Files
 files.forEach((file) => {
   shell.cp('-R', file, testFolder)
 })
@@ -21,5 +34,6 @@ folders.forEach((folder) => {
   shell.cp('-R', folder, testFolder)
 })
 
+// Copies CSS over and renames to ensure that the HTML served corresponds to the right name
 shell.cp('./dev/css/darkmode.css', testFolder + '/public/css/darkmode.min.css')
 shell.cp('./dev/css/style.css', testFolder + '/public/css/style.min.css')
